a54b0a93d173c1139ad4651a5cea9741f7a41589
Jokerguy46 <156993605+Jokerguy46@users.noreply.github.com>
2024-01-19 20:18:27 -0600

    added fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Your Name'
+description 'Description of your resource'
+version '1.0.0'
+
+client_scripts {
+    'client/hurricane_client.lua'
+}
+
+server_scripts {
+    'server/hurricane_server.lua'
+}
